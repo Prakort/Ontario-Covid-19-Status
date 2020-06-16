1ced3d08d7fd3bc38eb74403b87eb125c2eb86e8
Prakort <leanprakort@gmail.com>
2020-06-15 21:01:41 -0400

    status and confirmed cases

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,85 @@
-import urllib.request
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
+import pandas as pd
+import xmltodict
+import json 
+import urllib.request
+import requests
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+ 
+confirmed_url = 'https://data.ontario.ca/datastore/dump/455fd63b-603d-4608-8216-7d8647f43350?format=json'
+status_url = 'https://data.ontario.ca/datastore/dump/ed270bb8-340b-41f9-a7c6-e8ef587e6d11?format=xml'
 
-def fetchData():
-  url = 'https://data.ontario.ca/en/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=5'  
-  fileobj = urllib.request.urlopen(url)
-  print (fileobj.read())
+def fetchData(url):
+  # print("url", url)
 
-@app.route("/")
-def hello():
-    fetchData()
-    return "Hello World!"
+  # fileobj = urllib.request.urlopen(url2)
+  r = requests.get(url, stream = True)
+  # o = xmltodict.parse(r.content)
+  # print("r",r.content)
+
+  # b = json.dumps(o)
+  # print('json', b)
+  # return b
+  # json_content = pd.read_csv(r.content)
+  # json_content = pd.to_json()
+  # print('json', json_content)
+
+  # with open("455fd63b-603d-4608-8216-7d8647f43350.json",'wb') as f: 
+  
+  #   # Saving received content as a png file in 
+  #   # binary format 
+  
+  #   # write the contents of the response (r.content) 
+  #   # to a new file in binary mode. 
+  #   f.write(r.content) 
+  return r.content
+
+  
+def convertXML(data):
+  print("data", data)
+  parse = xmltodict.parse(data)
+  print("eoeoeoeoe")
+  return json.dumps(parse)
+  
+
+@app.route("/data", methods=['POST'])
+@cross_origin()
+def hello2():
+  try:
+    data = fetchData(confirmed_url)
+    result = json.loads(data)
+    return result
+  except:
+    return 'Error getting the data'
+   
+
+@app.route("/status", methods=['POST'])
+@cross_origin()
+def status_cases():
+  try:
+    data = fetchData(status_url)
+    return data
+  except:
+    return 'Error getting the status data'
+
+
+# @app.route("/")
+# @cross_origin()
+# def hello():
+#   try:
+#     data = fetchData()
+#     result = json.loads(data)
+#     # records = result['result']['records']
+#     # print('# of records', len(records))
+#     return result
+#   except:
+#     return 'Error getting the data'
+   
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Line } from 'react-chartjs-2';
+const Moment = require('moment');
+const MomentRange = require('moment-range');
+const moment = MomentRange.extendMoment(Moment);
+var parseString = require('xml2js').parseString;
 
+
+const data2 = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+ 
+  const fetchData  = async () => {
+    setLoading(true)
+    try {
+      const result = await axios.post('http://127.0.0.1:5000/status');
+      if (result.status === 200)
+        setData(result.data.records)
+
+      console.log("result", result.data)
+
+      const pastSeven = []
+      for ( var i = 0 ; i < 7 ;i++) {
+        let date = moment()
+        date = date.subtract(i, 'day').format('YYYY-MM-DD')
+        pastSeven.push(date)
+      }
+      var parser = new DOMParser();
+
+      // Use it to turn your xmlString into an XMLDocument
+      var xmlDoc = parser.parseFromString(result.data, "application/xml");
+      
+      var xml = "<root>Hello xml2js!</root>"
+      parseString( xmlDoc, function (err, res) {
+          console.log('convert to json',res);
+      });
+
+      // if (result.data) {
+      //   const this_month = '2020-06'
+      //   const this_month_data = result.data.records.filter(arr => arr[4].includes(this_month))
+      //   console.log("this month only data", this_month_data)
+      //   // const flattenData = []
+      //   const dateAndCount = {}
+      //   pastSeven.forEach( date => {
+      //     const count = result.data.records.filter(arr => arr[4].includes(date)).length;
+      //     dateAndCount[date] = count
+      //   })
+      
+
+      
+      // }
+     
+
+
+    } catch (err) {
+      console.log("error fetching data", err);
+    }
+    setLoading(false)
+  }
+
+
+
+  // console.log('past 7 days', pastSeven)
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +98,20 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
+        <Line 
+          styls={{backgroundColor:"white"}}
+          data={data2}
+          width={500}
+          height={500}
+        />
+
+        <button 
+          disabled={loading}
+          style={{ backgroundColor: 'white', height: 100, width: 100}}
+          onClick={() => fetchData()}
+        />
       </header>
     </div>
   );
